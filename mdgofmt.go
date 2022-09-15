35528f9195a0ed93f191fd2365aa8e0770ad834d
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2022-09-15 17:31:57 +0200

    string version

--- mdgofmt.go
+++ mdgofmt.go
@@ -1,8 +1,33 @@
 package mdgofmt
 
-import "go/format"
+import (
+	"fmt"
+	"go/format"
+	"strings"
+)
 
-func Format(md []byte) ([]byte, error) {
-	f, err := format.Source(md)
-	return f, err
+var (
+	snipStart = "```go\n"
+	snipEnd   = "\n```"
+)
+
+func Format(md string) (string, error) {
+	var (
+		out   strings.Builder
+		start = strings.Index(md, snipStart) + len(snipStart)
+		end   = strings.Index(md[start:], snipEnd) + start
+	)
+	fmt.Printf("start %d, end %d\n", start, end)
+
+	out.WriteString(md[:start])
+	// fmt.Println(out)
+	code := md[start:end]
+	fmt.Println("CODE", code)
+	formatted, err := format.Source([]byte(code))
+	if err != nil {
+		return "", err
+	}
+	out.Write(formatted)
+	out.WriteString(md[end:])
+	return out.String(), nil
 }
